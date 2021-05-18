5fb27c9f1b38a0b66fd2852b7db0abc4f9c66047
DireWolf707 <rahulsood707@gmail.com>
2021-05-18 17:51:53 +0530

    added template logic

--- weather/views.py
+++ weather/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-class HomeView(TemplateView):
-    template_name = 'home.html'
+def HomeView(request,):
+    import json
+    import requests
+    try:
+        api_req = requests.get('https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=E123B51F-9F55-4F10-A490-28AEA4ABC73C')
+        data = json.loads(api_req.content)
+    except:
+        data = None
+    
+    return render(request,'home.html',{'data':data})
     
 class AboutView(TemplateView):
     template_name = 'about.html'
